4dc0c2f448ea13c1fa506c11ce5af07d8e9aa2b1
GudvinPRO <138712942+GudvinPRO@users.noreply.github.com>
2024-01-11 19:54:49 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,8 @@
 const totalCategories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${totalCategories.length} .`);
 
+console.log(document.querySelectorAll("h2"));
+
 // const categoriesArray = [...totalCategories]
 //   .map(
 //     categories => Console.log(`Category: ${categories.children[0].textContent}
