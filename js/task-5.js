d1fa198cd0f94d0e1d9507382ee2bf9162075c08
GudvinPRO <138712942+GudvinPRO@users.noreply.github.com>
2024-01-27 18:10:10 +0200

    1

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,21 @@
+document.body.style.backgroundColor = '#a3a3a324';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+const textColor = document.querySelector('.color');
+const changeColorIn = document.querySelector('.change-color');
+const bodyStyle = document.body;
+
+
+const changeColor = () => {
+  textColor.textContent = getRandomHexColor();
+  bodyStyle.style.backgroundColor = textColor.textContent;
+};
+
+changeColorIn.addEventListener('click', changeColor);
